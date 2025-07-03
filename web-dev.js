6dfaf2a90fb5a08fa2b7b0506fc65f5d9fbe9e2a
OmkarTalekar06 <sanskrutitalekar1982@gmail.com>
2025-07-03 16:22:20 +0530

    adding web-dev

---
+++ web-dev.js
@@ -0,0 +1,110 @@
+let marks = 0;
+function chk() {
+    if (document.getElementById("unique").checked) {
+        marks += 2;
+        document.getElementById("Q1").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q1").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("stack").checked) {
+        marks += 2;
+        document.getElementById("Q2").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q2").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("media").checked) {
+        marks += 2;
+        document.getElementById("Q3").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q3").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("rem").checked) {
+        marks += 2;
+        document.getElementById("Q4").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q4").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("tag3").checked) {
+        marks += 2;
+        document.getElementById("Q5").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q5").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("child").checked) {
+        marks += 2;
+        document.getElementById("Q6").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q6").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("diff").checked) {
+        marks += 2;
+        document.getElementById("Q7").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q7").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("globe").checked) {
+        marks += 2;
+        document.getElementById("Q8").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q8").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("two").checked) {
+        marks += 2;
+        document.getElementById("Q9").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q9").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("close").checked) {
+        marks += 2;
+        document.getElementById("Q10").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q10").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("get").checked) {
+        marks += 2;
+        document.getElementById("Q11").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q11").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("NF").checked) {
+        marks += 2;
+        document.getElementById("Q12").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q12").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("fet").checked) {
+        marks += 2;
+        document.getElementById("Q13").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q13").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("cross").checked) {
+        marks += 2;
+        document.getElementById("Q14").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q14").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    if (document.getElementById("less").checked) {
+        marks += 2;
+        document.getElementById("Q15").style.backgroundColor ="rgba(26, 242, 26, 0.5)";
+    }
+    else{
+        document.getElementById("Q15").style.backgroundColor = " rgba(223, 27, 27, 0.5)";
+    }
+    
+    document.getElementById("res").innerHTML = marks + " marks";
+}
