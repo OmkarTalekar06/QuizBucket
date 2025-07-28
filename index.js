8579c74a85952956c4ce689db54a9220dd8b5e29
OmkarTalekar06 <sanskrutitalekar1982@gmail.com>
2025-07-28 14:43:17 +0530

    update

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 function enter_name(){
     let user_name = prompt("Please enter your name : ");
-<<<<<<< HEAD
-    localStorage.setItem("name", user_name)
-=======
+    
+    if (!user_name) {
+        alert("Please Enter your name as it's necessary for certification");
+        return;
+    }
+
+    // Save in localStorage
+    localStorage.setItem("name", user_name);
+
+    // Send to backend
+    fetch("https://python-wwwi.onrender.com/sendname", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({name: user_name})
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+    });
+
+    alert("Welcome, " + user_name);
     return user_name;
 }
-function get_name1(){
-    let user_name = enter_name();
-    if(!user_name){
-         alert("Please Enter your name as its necessary for certification");
-         return;
-    }
-    alert("Welcome, " + user_name)
 
->>>>>>> 1bfbcb0 ("update')
-    if(user_name){
-        fetch("https://python-wwwi.onrender.com/sendname", {
-            method:'POST',
-            headers:{'Content-Type':'application/json'},
-            body:JSON.stringify({name: user_name})
-        })
-        .then(response => response.json())
-        .then(data => {
-            alert(data.message);
-        });
-    }
-}
