e716a78f946fac491cc36c2e78a1114f8bff5322
OmkarTalekar06 <sanskrutitalekar1982@gmail.com>
2025-07-28 14:40:22 +0530

    update

--- cpp.js
+++ cpp.js
@@ -112,9 +112,13 @@ function chk() {
     for (let i=0; i < allAnswers.length; i++){
         allAnswers[i].style.display = "block";
     }
+<<<<<<< HEAD
     
     localStorage.setItem("marks", marks)
     sendMarks();
+=======
+    localStorage.setItem("marks", marks)
+>>>>>>> 1bfbcb0 ("update')
     console.log("Script Loaded");
 
 }
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-function get_name1() {
+function enter_name(){
     let user_name = prompt("Please enter your name : ");
+<<<<<<< HEAD
     localStorage.setItem("name", user_name)
+=======
+    return user_name;
+}
+function get_name1(){
+    let user_name = enter_name();
+    if(!user_name){
+         alert("Please Enter your name as its necessary for certification");
+         return;
+    }
+    alert("Welcome, " + user_name)
+
+>>>>>>> 1bfbcb0 ("update')
     if(user_name){
         fetch("https://python-wwwi.onrender.com/sendname", {
             method:'POST',
